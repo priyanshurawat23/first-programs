e411cbf0b183478f71fbc90c51c7f2d9d13de2ea
Priyanshu Rawat <priyanshu.r2309@gmail.com>
2023-12-21 00:41:59 +0530

    Update str is palindrome or not 1st code.py

--- str is palindrome or not 1st code.py
+++ str is palindrome or not 1st code.py
@@ -4,7 +4,7 @@
 str1=input('enter a string---> ')
 
 #replacing any spaces with commas and converting all alphabets to lowercase
-d= str1.replace(",").lower()
+d= str1.replace(",").replace(";").replace(":").replace("!").replace("?").replace(".").replace("-").replace("'").replace(" ").lower()
 
 #if-else loop for execution of purpose
 if d==d[::-1]:
